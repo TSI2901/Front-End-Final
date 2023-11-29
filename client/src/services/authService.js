39096e045d70b41e124e20816b9ee3770154512c
Anton <tonkata2007@gmail.com>
2023-11-29 12:25:33 +0200

    adding service

---
+++ client/src/services/authService.js
@@ -0,0 +1,26 @@
+import * as request from "../lib/request";
+
+const BaseUrl = "http://localhost:3030/users";
+
+export async function login(email, password) {
+    const result = await request.post(`${BaseUrl}/login`, {
+        email,
+        password
+    });
+
+    return result;
+}
+
+export async function register(email, password, username) {
+    const result = await request.post(`${BaseUrl}/register`, {
+        email,
+        password,
+        username
+    });
+
+    return result;
+}
+
+export async function logout() {
+    await request.get(`${BaseUrl}/logout`);
+}
