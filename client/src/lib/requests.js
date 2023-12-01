d3bd7b60333e4198d566c2f416813ab71c84da1d
Anton <tonkata2007@gmail.com>
2023-12-01 13:36:16 +0200

    server requests

---
+++ client/src/lib/requests.js
@@ -0,0 +1,51 @@
+const baseUrl = 'http://localhost:3030/data/movies';
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    const data = Object.values(result);
+
+    return data;
+};
+
+export const getOne = async (movieId) => {
+    const response = await fetch(`${baseUrl}/${movieId}`);
+    const result = await response.json();
+
+    return result;
+};
+
+export const create = async (data) => {
+    const body = {
+        _ownerId: data.ownerId,
+        title: data.title,
+        description: data.description,
+        img: data.img,
+        _createdOn: new Date(data.createdOn),
+        _id: data.id
+    };
+
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    })
+
+    const result = await response.json();
+    console.log(result);
+
+    return result;
+};
+
+export const remove = async (movieId) => {
+    const response = await fetch(`${baseUrl}/${movieId}`, {
+        method: 'DELETE'
+    });
+
+    const result = await response.json();
+
+    return result;
+};
