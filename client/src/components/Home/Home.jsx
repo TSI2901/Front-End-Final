2e390333b90bf591d691f2d9f7dd9049b60a7fa4
Anton <tonkata2007@gmail.com>
2023-12-08 22:53:48 +0200

    Home view

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+
+export default function Home(){
+    return (
+        <main>
+        <section class="featured-movies">
+            <h1>The right place to choose a movie for you!</h1>
+          
+        </section>
+    </main>
+
+
+    )
+}
