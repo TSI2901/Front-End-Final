5b78d49533d543f1d04b50daeb6ddfb20a176d32
Anton <tonkata2007@gmail.com>
2023-12-08 22:53:23 +0200

    Footer view

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer >
+            <div className="container">
+                <span className="footer-text">&copy; 2023 - MovieHub</span>
+            </div>
+        </footer>
+    )
+}   
