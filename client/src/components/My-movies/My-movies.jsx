a7eba422d8d78c51fefc19835e98ee91f08d9ffa
Anton <tonkata2007@gmail.com>
2023-12-08 22:56:21 +0200

    Personal section view

---
+++ client/src/components/My-movies/My-movies.jsx
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from 'react'
+import CatalogItem from '../CatalogItem/CatalogItem.jsx';
+import './Catalog.css'
+
+import { getAllByOwner } from '../../services/movieService.js';
+
+import { formatDate } from '../../utils/dataUtil.js';
+
+import  AuthContext  from '../../context/authContext.jsx'
+
+ 
+
+function Catalog() {
+    const [movies, setMovies] = useState([]);
+    const {userId} = useContext(AuthContext)
+    useEffect(() => {
+        setIsLoading(true);
+
+        getAllByOwner(userId)
+            .then(result => setMovies(result))
+            .catch(err => console.log(err))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    
+    return(
+        <div className="catalog">
+            <div className="catalog-item">
+            
+            {movies.map(movie =>(
+                <CatalogItem
+                key={movie._id}
+                userId={userId}
+                title={movie.title}
+                imgUrl = {movie.img}
+                ownerId={movie._ownerId}
+                id = {movie._id}
+                createdOn={formatDate(movie._createdOn)}
+                />
+            ))}     
+            </div>
+        </div>
+    )
+}
+export default Catalog
