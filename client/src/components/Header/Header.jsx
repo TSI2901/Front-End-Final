63d551e4df30ae21b2b96fddcca785b285cb0d7e
Anton <tonkata2007@gmail.com>
2023-12-08 22:51:18 +0200

    Header view

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,60 +1,47 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/authContext';
+import { useNavigate } from 'react-router-dom';
 
-import AuthContext from '../../contexts/authContext';
-
-import "./Header.css";
+import './Header.css'
 
 export default function Header() {
-  const { username, isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+    const { userId } = useContext(AuthContext);
+    if (userId !== undefined){
+       return (
+           <header>
+               <img onClick={()=>{navigate(`/`)}} src="../../../public/toni_logo.png" className="img"/>
+               <div className="header">
+                   <div>
+                   <Link to={`/Catalog`} className="nav-button">Catalog</Link>
+               </div>
+            <nav>
 
-  return (
-    <header>
-      <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom box-shadow mb-3 color-orange">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img src="./public/big-logo.png" className="logo" alt="Logo" />
-          </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-            <ul className="navbar-nav flex-grow-1">
-              <li className="nav-item">
-                <Link className="nav-link text-dark" to="/reviews">Reviews</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-dark" to="/add">Add</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-dark" to="/about">About</Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav">
-              {!isAuthenticated ? (
-                <>
-                  <li className="nav-item">
-                    <Link className="nav-link text-dark" to="/login">Login</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link text-dark" to="/register">Register</Link>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item d-none d-sm-block">
-                    <span className="nav-link text-dark">Hello, {username}!</span>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link text-dark" to="/logout">Logout</Link>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+                <div id="user">
+                    <Link to="/Add" className="nav-button">Add Movie</Link>
+                    <Link to="/MyMovies" className="nav-button">My Movies</Link>
+                    <Link to="/Logout" className="nav-button">Logout</Link>
+                </div>
+            </nav>
+               </div>
+        </header>
+       )
+    }
+    else {
+        return (
+            <header>
+                <img onClick={()=>{navigate(`/`)}} src="../../../public/toni_logo.png" className="img"/>
+                <div className="header">
+                    <Link to={`/Catalog`} className="nav-button">Catalog</Link>
+                <nav>
+                    <div id="guest">
+                        <Link to="/Login" className="nav-button">Login</Link>
+                        <Link to="/Register" className="nav-button">Register</Link>
+                    </div>
+                </nav>
+                </div>
+            </header>
+        );
+    }
 }
